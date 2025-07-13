ea529c3fe8cd9d47f8d833dcbe2b6e4b3542d591
miku <7517707+BaroqueEngine@users.noreply.github.com>
2025-07-14 02:41:08 +0900

    update 414/c

--- abc/414/C/0.py
+++ abc/414/C/0.py
@@ -1,28 +1,27 @@
 A = int(input())
 N = int(input())
 
-def check(x, radix):
-    ret = []
-    while x > 0:
-        ret.append(x % radix)
-        x //= radix
-    ret.reverse()
-    return ret == ret[::-1]
-
 nums = set([])
 for i in range(1, 1000000):
-    si = str(i)
-    x = int(si + si[::-1])
-    if x <= N:
-        nums.add(x)
-    a = si[:-1]
-    x = int(a + si[-1] + a[::-1])
-    if x <= N:
-        nums.add(x)
+    s1 = int(str(i) + str(i)[::-1])
+    s2 = int(str(i) + str(i)[:-1][::-1])
+
+    if s1 <= N:
+        nums.add(s1)
+    
+    if s2 <= N:
+        nums.add(s2)
+
+def is_palindrome(x):
+    ret = []
+    while x > 0:
+        ret.append(x % A)
+        x //= A
+    return ret == ret[::-1]
 
 ans = 0
 for x in nums:
-    if check(x, A):
+    if is_palindrome(x):
         ans += x
 
 print(ans)
